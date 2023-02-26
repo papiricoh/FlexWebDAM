d32945230b5d0869f68d6f67ff83beb1c342a6c2
Papiricoh <slendeman0200@gmail.com>
2023-02-26 20:36:43 +0100

    Added funtional mobileMenu

--- js/index.js
+++ js/index.js
@@ -1,22 +1,11 @@
 var hamburguer = document.querySelector('#menu');
 
 hamburguer.addEventListener('click', function(e) {
-    var logo = document.querySelector('#logo');
-    if (logo.style.display === 'none') {
-        logo.style.display = 'block';
-    } else {
+    var logo = document.querySelector('.mobile_menu');
+    if (logo.style.display !== 'none') {
         logo.style.display = 'none';
-    }
-    var subMenu = document.querySelectorAll('.display_button');
-    for (let index = 0; index < subMenu.length; index++) {
-        if (subMenu[index].style.display == 'none') {
-            subMenu[index].style.display = 'block';
-        } else {
-            subMenu[index].style.display = 'none';
-        }
+    } else {
+        logo.style.display = 'flex';
     }
 
-
-
-    e.preventDefault();
 });
